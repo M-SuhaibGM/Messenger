9c95210ce6d89f9dcb214ff54c6160971f1122c8
M-SuhaibGM <mmsohaib617@gmail.com>
2025-04-10 18:00:12 +0500

    fix3

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
-// app/api/auth/[...nextauth]/route.ts
+
 import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth";
+import { authOptions } from "@/lib/auth"; // Import from your config file
 
 const handler = NextAuth(authOptions);
 
