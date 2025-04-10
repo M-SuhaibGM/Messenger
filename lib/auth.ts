158771f24f4957dfb04aff3ddf7ae2dc11fce409
M-SuhaibGM <mmsohaib617@gmail.com>
2025-04-10 18:12:05 +0500

    fix4

--- lib/auth.ts
+++ lib/auth.ts
@@ -8,7 +8,7 @@ import { db } from "@/lib/db";
 
 declare module "next-auth" {
   interface Session {
-    user: {
+    user: {       
       id: string;
       name?: string | null;
       email?: string | null;
